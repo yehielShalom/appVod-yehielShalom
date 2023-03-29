a93c447b36459c9866332957357f406616a435d5
Yehiel shalom <y6112000sha@gmail.com>
2023-03-29 14:50:29 +0300

    ik

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { Carousel } from "flowbite";
 
 const NavBar = () => {
   return (
     <div>
       <div className="upNav">
-        <div className="logoAndSearch">
+        <div className="logoAndSearch flex justify-between m-5">
           <h1>logo</h1>
           <div className="form-control">
             <div className="input-group">
-              <input
-                type="text"
-                placeholder="Search…"
+              <input   type="text" placeholder="Search…"
                 className="input input-bordered input-sm"
               />
               <button className="btn btn-square btn-sm">
@@ -32,6 +31,38 @@ const NavBar = () => {
             </div>
           </div>
         </div>
+        <div className="carousel">
+        <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://images.pexels.com/photos/80474/grass-snake-snake-serpentes-natrix-80474.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://images.pexels.com/photos/45246/green-tree-python-python-tree-python-green-45246.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://images.pexels.com/photos/80474/grass-snake-snake-serpentes-natrix-80474.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://images.pexels.com/photos/45246/green-tree-python-python-tree-python-green-45246.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>    
+        </div> 
       </div>
       <div className="downNav"></div>
     </div>
