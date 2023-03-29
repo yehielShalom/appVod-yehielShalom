9fccb2593fd4cdb249b50fc40c781527e9935977
Yehiel shalom <y6112000sha@gmail.com>
2023-03-29 15:59:38 +0300

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"),  require('flowbite/plugin')],
 
   // daisyUI config (optional)
   daisyui: {
---
+++ test.jsx
@@ -0,0 +1,58 @@
+// {
+//     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+//   <Carousel>
+//     <img
+//       src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+//       alt="..."
+//     />
+//     <img
+//       src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+//       alt="..."
+//     />
+//     <img
+//       src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+//       alt="..."
+//     />
+//     <img
+//       src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+//       alt="..."
+//     />
+//     <img
+//       src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+//       alt="..."
+//     />
+//   </Carousel>
+// </div>
+// }
+// {
+//     <div className="carousel w-full h-[250px]">
+//   <div id="slide1" className="carousel-item relative w-full">
+//     <img src="https://images.pexels.com/photos/80474/grass-snake-snake-serpentes-natrix-80474.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+//     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//       <a href="#slide4" className="btn btn-circle">❮</a> 
+//       <a href="#slide2" className="btn btn-circle">❯</a>
+//     </div>
+//   </div> 
+//   <div id="slide2" className="carousel-item relative w-full">
+//     <img src="https://images.pexels.com/photos/45246/green-tree-python-python-tree-python-green-45246.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+//     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//       <a href="#slide1" className="btn btn-circle">❮</a> 
+//       <a href="#slide3" className="btn btn-circle">❯</a>
+//     </div>
+//   </div> 
+//   <div id="slide3" className="carousel-item relative w-full">
+//     <img src="https://images.pexels.com/photos/80474/grass-snake-snake-serpentes-natrix-80474.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+//     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//       <a href="#slide2" className="btn btn-circle">❮</a> 
+//       <a href="#slide4" className="btn btn-circle">❯</a>
+//     </div>
+//   </div> 
+//   <div id="slide4" className="carousel-item relative w-full">
+//     <img src="https://images.pexels.com/photos/45246/green-tree-python-python-tree-python-green-45246.jpeg?auto=compress&cs=tinysrgb&w=1600" className="w-full" />
+//     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//       <a href="#slide3" className="btn btn-circle">❮</a> 
+//       <a href="#slide1" className="btn btn-circle">❯</a>
+//     </div>
+//   </div>
+// </div>
+// }
