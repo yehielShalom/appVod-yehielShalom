ff7489a46f35fb85111e2717039ca65ce12e926b
Yehiel shalom <y6112000sha@gmail.com>
2023-04-16 11:46:11 +0300

    v

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import * as te from 'tw-elements';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
+  <Provider></Provider>
+      <App />
+  
 )
