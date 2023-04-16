a93732575823de630d1db05b33e0394e6cb3806a
Yehiel shalom <y6112000sha@gmail.com>
2023-04-16 14:16:56 +0300

    l

--- src/app/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import dataReducer from "../featutes/data/DataSlice"
-
-export default configureStore({
-    reducer:{
-      dataReducer  
-    }
-})
